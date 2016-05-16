5718f4368c7cd6ec837f8bc3cfc9dd6c76e457bb
Ramkumar Rao <rao.ram5718@gmail.com>
2016-05-16 11:59:56 -0400

    Bug Fix and Test Cases
    Fixed a bug and added some test cases

--- test/test.js
+++ test/test.js
@@ -1,36 +1,82 @@
 var assert = require('chai').assert;
-var opkit = require('../opkit');
-var AWS = require('../node_modules/aws-sdk');
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+var opkit = require('../index');
+var object = new opkit.SQS();
+var alarms = new opkit.Alarms();
+//var object = new opkit();
+var sinon = require('sinon');
 
-/*describe('getSQSQueueSizeNotVisibleData', function() {
-	it('verifies success of SQS Queue sizes', function() {
-		var url = "testID";
-		opkit.updateAcessKeyID(access);
-		assert.equal(access,true);
+/**** Example
+function once(fn) {
+    var returnValue, called = false;
+    return function () {
+        if (!called) {
+            called = true;
+            returnValue = fn.apply(this, arguments);
+        }
+        return returnValue;
+    };
+}
+
+it("calls the original function", function () {
+    var spy = sinon.spy();
+    var proxy = once(spy);
+
+    proxy();
+
+    assert(spy.called);
+});
+****/ 
+
+//Mocking out functions
+beforeEach(function() {
+	sinon.stub(object, 'retrieveSQSQueueData', function (url, param, auth, callback) {
+		callback(null,'success.');
 	});
-});*/
+
+	sinon.stub(object, 'getSQSQueueSizeInt', function(url, auth, callback) {
+		object.retrieveSQSQueueData(url, 'ApproximateNumberOfMessages', auth, callback);
+	});
+	
+	sinon.stub(object, 'getSQSQueueSizeNotVisibleInt', function(url, auth, callback) {
+		object.retrieveSQSQueueData(url, 'ApproximateNumberOfMessagesNotVisible', auth, callback);
+	});
+	
+	//Not sure how to stub out promises
+	/*sinon.stub(alarms, 'healthReportByState', function(auth) {
+		return new Promise(function(resolve) {
+			return "success";
+		});
+	});*/
+});
+
+//Executed after tests
+afterEach(function() {
+	object.retrieveSQSQueueData.restore();
+	object.getSQSQueueSizeInt.restore();
+	object.getSQSQueueSizeNotVisibleInt.restore();
+	//alarms.healthReportByState.restore();
+});
+
+it("getSQSQueueSizeInt successfully makes a callback", function() {
+	var spy = sinon.spy();
+	var proxy = object.getSQSQueueSizeInt("Example", "d", spy);
+	
+	assert(spy.called);
+});
+
+it("getSQSQueueSizeNotVisibleInt successfully makes a callback", function() {
+	var spy = sinon.spy();
+	var proxy = object.getSQSQueueSizeNotVisibleInt("Example", "d", spy);
+	
+	assert(spy.called);
+});
 
 /*
-describe('getSQSQueueSizeInt', function() {
-	it('verifies success of retrieveing SQS queue size', function(done) {
-		opkit.updateAuthKeys('AKIAJOQCFVMRLLNFWLZA','bHXuXbdF9tg7NJjCN2PutXjCJnkCV+Cb/0vJPn7F');
-		opkit.getSQSQueueSizeInt('https://sqs.us-east-1.amazonaws.com/848840820992/nguyer-sms-queue', function(err, data) {
-			var x = data;
-		});
-		assert.equal(x,2);
-		done();
-		
-	});
+it("Does a simple test", function() {
+	var spy = sinon.spy();
+	var proxy = alarms.healthReportByState("test");
+	proxy.then(spy);
+	
+	assert(spy.called);
 });
 */
-
-var assert = require('chai').assert;
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
