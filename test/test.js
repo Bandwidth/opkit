36e2d6682fafa3f2687002717ff5a611b0fa9612
Ramkumar Rao <rao.ram5718@gmail.com>
2016-05-16 16:57:20 -0400

    Removed Unnecessary Comments

--- test/test.js
+++ test/test.js
@@ -1,35 +1,10 @@
 var assert = require('chai').assert;
 var opkit = require('../index');
 var sqsqueue = new opkit.SQS();
-var alarms = new opkit.Alarms();
-//var object = new opkit();
 var sinon = require('sinon');
 var AWSMock = require('aws-sdk-mock');
 var AWS = require('aws-promised');
 
-/**** Example
-function once(fn) {
-    var returnValue, called = false;
-    return function () {
-        if (!called) {
-            called = true;
-            returnValue = fn.apply(this, arguments);
-        }
-        return returnValue;
-    };
-}
-
-it("calls the original function", function () {
-    var spy = sinon.spy();
-    var proxy = once(spy);
-
-    proxy();
-
-    assert(spy.called);
-});
-****/ 
-
-//Mocking out functions
 describe('Opkit testing', function() {
 
 	describe('SQS functions', function() {
@@ -47,7 +22,6 @@ describe('Opkit testing', function() {
 		});
 
 		afterEach(function() {
-			//sqsqueue.retrieveSQSQueueData.restore();
 			AWSMock.restore('SQS', 'getQueueAttributes');
 		});
 
@@ -65,8 +39,5 @@ describe('Opkit testing', function() {
 			assert(spy.calledWith(null, 0));
 		});
 	});
-	
-	/**** Some other testing unrelated to SQSQueues goes here - should be unaffected by mocks made eariler ****/
-	
 });
 
