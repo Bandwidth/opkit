88f329a9405b37fc9e627e038a77f1f1409de4c5
Illirik Smirnov <ims@live.unc.edu>
2016-05-19 08:54:06 -0400

    Add files via upload

--- publish.sh
+++ publish.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	cd $HOME
-	git config --global user.email "travis@travis-ci.org"
-	git config --global user.name "travis-ci"
-	git clone --quiet --branch=gh-pages git@github.com:inetCatapult/opkit.git
+cp -Rf out/ $HOME/
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages git@github.com:inetCatapult/opkit.git
 
-	cd gh-pages
-	git rm -rf ./javadoc
-	cp -Rf $HOME/out ./doc
-	git add -f .
-	git commit -m "Generated docs from Travis."
-	git push -fq origin gh-pages > /dev/null
-fi
+cd gh-pages
+git rm -rf ./javadoc
+cp -Rf $HOME/out ./doc
+git add -f .
+git commit -m "Generated docs from Travis."
+git push -fq origin gh-pages > /dev/null
