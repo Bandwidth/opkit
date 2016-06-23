7b3626bb7dd6a50f3226562860e6f8b08bd4d8d9
ismirnov <ims@live.unc.edu>
2016-06-23 15:57:56 -0400

    The sample script now works. It was like three refactors behind.

--- samples/sample.js
+++ samples/sample.js
@@ -1,36 +1,60 @@
 var Opkit = new require('../index');
-var opkit = new Opkit();
-var Bot = Opkit.Bot
-console.log(Bot);
+var Bot = Opkit.Bot;
+var Alamrs = Opkit.Alarms;
 var BOT_NAME = 'samplebot';
+var persister = new Opkit.Persister('./state');
+var commands = [];
 
-var state = {
-	numberOfMessages : 0
-};
-
-var commands = { 
-	status : function(message, bot, auth){
-		bot.state.numberOfMessages = bot.state.numberOfMessages + 1;
-		if (auth.props){
-			bot.sendMessage(self.opkit.Alarms.queryAlarmsByStateReadably(auth), message.channel);
+var health = {
+	name : 'health',
+	script : 'sample',
+	syntax: ['status', 'health'],
+	command : function(message, bot, auth, brain){
+		brain.numberOfMessages = brain.numberOfMessages + 1 || 1;
+		if (process.env.amazonId && process.env.amazonSecret){
+			return Alarms.healthReportByState(
+				new Opkit.Auth(process.env.amazonId,
+				process.env.amazonSecret, 'us-east-1'))
+			.then(function(data){
+				bot.sendMessage(data.replace(/(.*)([:])/mg, 
+					function(match) { return '*' + match + '*'; }), message.channel);
+				return Promise.resolve('User ' + bot.dataStore.getUserById(message.user).name + 
+					' queried for a health report.');
+			});
 		} else {
 			bot.sendMessage("You haven't got AWS keys defined as environment variables!\n"+
 							"Ensure that your AWS keys are in the following places:\n"+
 							"amazonId : Access Key ID\n"+
 							"amazonSecret : Secret Access Key\n"+
 							"amazonRegion : Region (e.g. 'us-east-1').", message.channel);
+			return Promise.resolve('User ' + bot.dataStore.getUserById(message.user).name + 
+				' queried for a health report.');
 		}
-	},
-	joke : function(message, bot){
-		bot.state.numberOfMessages = bot.state.numberOfMessages + 1;
+	}
+}
+
+var joke = {
+	name : 'joke',
+	script : 'sample',
+	syntax : ['joke'],
+	command : function(message, bot, auth, brain){
 		bot.sendMessage("A burrito is a sleeping bag for ground beef.", message.channel);
-	},
-	count : function(message, bot){
-		bot.state.numberOfMessages = bot.state.numberOfMessages + 1;
-		bot.sendMessage("You've sent me " + bot.state.numberOfMessages + " messages, including that one.",
+	}
+}
+
+var count = {
+	name : 'count',
+	script : 'count',
+	syntax : ['count'],
+	command : function(message, bot, auth, brain){
+		brain.numberOfMessages = brain.numberOfMessages + 1 || 1;
+		bot.sendMessage(brain.numberOfMessages + '!',
 			message.channel);
 	}
 };
 
-bot = opkit.makeBot(BOT_NAME, commands, state);
+commands.push(health);
+commands.push(joke);
+commands.push(count);
+bot = new Bot(BOT_NAME, commands, persister);
 bot.start();
